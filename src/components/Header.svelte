b4396b23a45d07f25b5073428a11403eb86ba374
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-09-07 22:20:50 -0400

    make header elements block on mobile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,11 +3,11 @@
 	import { socials } from '$lib/store';
 </script>
 
-<div class="flex justify-between items-center w-full py-5">
+<div class="md:flex justify-between items-center w-full py-5">
 	<a href="/">
 		<img src="/images/logo.svg" alt="logo" class="w-16" />
 	</a>
-	<div class="flex justify-center space-x-5">
+	<div class="mt-5 md:mt-0 md:flex justify-center space-x-5">
 		<SocialLink url={$socials.twitter} social="twitter" />
 		<SocialLink url={$socials.discord} social="discord" />
 		<SocialLink url={$socials.github} social="github" />
