4eac02afc6c75f0680a80dea2f6e5be1626aa912
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-09-10 20:13:03 -0600

    finish hooking up new add location page and design

--- src/components/PrimaryButton.svelte
+++ src/components/PrimaryButton.svelte
@@ -6,6 +6,7 @@
 	export let type = undefined;
 	export let external = undefined;
 	export let disabled = undefined;
+	export let loading = undefined;
 </script>
 
 {#if link}
@@ -24,6 +25,22 @@
 		{disabled}
 		class="block bg-link hover:bg-hover text-white text-center font-semibold rounded-xl {style}"
 	>
-		{text}
+		{#if loading}
+			<svg
+				class="animate-spin h-6 w-6 text-white mx-auto"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+				<path
+					class="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+				/>
+			</svg>
+		{:else}
+			{text}
+		{/if}
 	</button>
 {/if}
---
+++ src/routes/add-location/endpoint/+server.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { AIRTABLE_API_KEY } from '$env/static/private';
+
+export async function POST({ request }) {
+	const headers = {
+		Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+		'Content-Type': 'application/json'
+	};
+
+	let { name, address, url, type, methods, twitter, notes } = await request.json();
+
+	let airtable = await axios
+		.post(
+			'https://api.airtable.com/v0/app4wIiES4dsRLIrH/Locations',
+			{
+				fields: {
+					Name: name,
+					Address: address,
+					URL: url,
+					Type: type,
+					fldsVjrsPyMKV7ro1: methods,
+					fldfXqVj05zgONpHv: twitter,
+					Notes: notes,
+					Status: 'Todo',
+					fldgbuR3ugA8K46uJ: new Date(Date.now()).toISOString()
+				}
+			},
+			{ headers }
+		)
+		.then(function (response) {
+			return response.data;
+		})
+		.catch(function (error) {
+			console.log(error);
+			throw new Error(error);
+		});
+
+	return new Response(JSON.stringify(airtable));
+}
