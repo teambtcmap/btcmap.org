72ff4829ce4f85a385996b9f03bebed284b75d4f
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-09-19 10:32:28 -0600

    add ln address to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,5 +4,7 @@
 			href="bitcoin:bc1qng60mcufjnmz6330gze5yt4m6enzra7lywns2d"
 			class="text-link hover:text-hover">bc1qng60mcufjnmz6330gze5yt4m6enzra7lywns2d</a
 		>
+		|
+		<a href="lightning:btcmap@zbd.gg" class="text-link hover:text-hover">btcmap@zbd.gg</a>
 	</p>
 </footer>
