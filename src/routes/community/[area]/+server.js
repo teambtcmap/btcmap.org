b137fa8fa249a1162b2a369bb710a2b5e77c5a76
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-10-03 10:11:54 -0600

    refactor communities endpoint to use less computation

--- src/routes/community/[area]/+server.js
+++ src/routes/community/[area]/+server.js
@@ -4,16 +4,16 @@ import axios from 'axios';
 export async function GET({ params }) {
 	let { area } = params;
 
-	const response = await axios.get('https://api.btcmap.org/areas');
+	const response = await axios.get(`https://api.btcmap.org/areas/${area}`);
 
-	response.data.forEach((record) => {
-		if (record.id === area) {
-			throw redirect(
-				302,
-				`/map?lat=${record['min_lat']}&long=${record['min_lon']}&lat=${record['max_lat']}&long=${record['max_lon']}`
-			);
-		}
-	});
+	const data = response.data;
+
+	if (data) {
+		throw redirect(
+			302,
+			`/map?lat=${data['min_lat']}&long=${data['min_lon']}&lat=${data['max_lat']}&long=${data['max_lon']}`
+		);
+	}
 
 	throw error(404, 'Not Found');
 }
