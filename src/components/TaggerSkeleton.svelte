da92a30a1cc9a801901472002adbf1f3f91773dc
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-10-08 11:24:00 -0600

    improve latest supertagger skeleton to match closer to actual comp

--- src/components/TaggerSkeleton.svelte
+++ src/components/TaggerSkeleton.svelte
@@ -1,10 +1,18 @@
 <div
 	class="md:flex justify-between space-y-2 md:space-y-0 md:space-x-5 lg:space-x-0 items-center p-5"
 >
+	<div class="space-y-2 md:space-y-0 md:space-x-5 md:flex items-center">
+		<!-- dot placeholder -->
+		<div class="h-3 w-3 mx-auto md:mx-0 animate-pulse rounded-full bg-statPositive/50" />
+
+		<!-- event placeholder -->
+		<div
+			class="h-[38px] md:h-[30px] w-full md:w-[450px] mx-auto md:mx-0 animate-pulse rounded-xl bg-statPositive/50"
+		/>
+	</div>
+
+	<!-- tipping button placeholder -->
 	<div
-		class="h-[30px] w-full md:w-[400px] mx-auto md:mx-0 animate-pulse rounded-xl bg-statPositive/50"
-	/>
-	<div
-		class="h-[30px] w-1/2 md:w-[220px] mx-auto md:mx-0 animate-pulse rounded-xl bg-statPositive/50"
+		class="h-[38px] md:h-[30px] w-full md:w-20 mx-auto md:mx-0 animate-pulse rounded-lg bg-statPositive/50"
 	/>
 </div>
