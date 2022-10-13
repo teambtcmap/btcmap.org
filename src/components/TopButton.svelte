84ef6bf03125e0cf5f5b9bc2341d98ecf859aa3d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-10-12 21:26:36 -0600

    create top button component

---
+++ src/components/TopButton.svelte
@@ -0,0 +1,6 @@
+<button
+	on:click={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+	class="animate-bounce w-8 h-8 bg-link hover:bg-hover rounded-full text-white"
+>
+	<i class="fa-solid fa-chevron-up" />
+</button>
