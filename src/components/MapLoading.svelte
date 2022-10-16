9b5be7939b2dfe09054dfc4f0db764159fc7b2b1
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-10-16 11:53:44 -0600

    add attributes to img to make it appear faster and sync

--- src/components/MapLoading.svelte
+++ src/components/MapLoading.svelte
@@ -7,7 +7,15 @@
 	<div class="w-10/12 xl:w-[1200px] mx-auto">
 		<div class="h-[100vh] w-full flex justify-center items-center">
 			<main class="space-y-12">
-				<a href="/"><img src="/images/logo.svg" alt="BTC Map" class="w-36 md:w-40 mx-auto" /></a>
+				<a href="/">
+					<img
+						decoding="sync"
+						fetchpriority="high"
+						src="/images/logo.svg"
+						alt="BTC Map"
+						class="w-36 md:w-40 mx-auto"
+					/>
+				</a>
 				<h1 class="text-primary font-semibold text-4xl md:text-5xl text-center">{message}</h1>
 			</main>
 		</div>
