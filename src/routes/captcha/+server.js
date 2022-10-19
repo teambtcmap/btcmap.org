5c5144612bda053e85c6134aaa554ca2d8437c91
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-10-19 09:45:35 -0600

    dont allow for captcha reuse

--- src/routes/captcha/+server.js
+++ src/routes/captcha/+server.js
@@ -1,8 +1,28 @@
 import { json } from '@sveltejs/kit';
-import { generate } from '$lib/captcha';
+import crypto from 'crypto';
+import svgCaptcha from 'svg-captcha';
+import { SERVER_CRYPTO_KEY, SERVER_INIT_VECTOR } from '$env/static/private';
+import { error } from '@sveltejs/kit';
 
 // generate and return captcha
 export function GET() {
-	let captcha = generate();
-	return json(captcha);
+	const initVector = Buffer.from(SERVER_INIT_VECTOR, 'hex');
+	const serverKey = Buffer.from(SERVER_CRYPTO_KEY, 'hex');
+
+	svgCaptcha.options.width = 275;
+	svgCaptcha.options.height = 100;
+	svgCaptcha.options.fontSize = 100;
+
+	let captcha = svgCaptcha.create({ size: 7, noise: 2 });
+
+	if (!captcha.data) {
+		throw error(400, 'Could not generate captcha, please try again or contact BTC Map.');
+	}
+
+	let encrypt = crypto.createCipheriv('aes-256-cbc', serverKey, initVector);
+
+	let secret = encrypt.update(captcha.text, 'utf8', 'hex');
+	secret += encrypt.final('hex');
+
+	return json({ captcha: captcha.data, captchaSecret: secret });
 }
--- src/routes/report-outdated-info/endpoint/+server.js
+++ src/routes/report-outdated-info/endpoint/+server.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { GITHUB_API_KEY } from '$env/static/private';
-import { verify } from '$lib/captcha';
+import crypto from 'crypto';
+import { SERVER_CRYPTO_KEY, SERVER_INIT_VECTOR } from '$env/static/private';
+import { error } from '@sveltejs/kit';
+
+let used = [];
 
 export async function POST({ request }) {
 	const headers = {
@@ -28,7 +32,23 @@ export async function POST({ request }) {
 	}
 
 	// verify that captcha is correct
-	verify(captchaSecret, captchaTest);
+	const initVector = Buffer.from(SERVER_INIT_VECTOR, 'hex');
+	const serverKey = Buffer.from(SERVER_CRYPTO_KEY, 'hex');
+
+	let decrypt = crypto.createDecipheriv('aes-256-cbc', serverKey, initVector);
+
+	let secret = decrypt.update(captchaSecret, 'hex', 'utf8');
+	secret += decrypt.final('utf8');
+
+	if (captchaTest !== secret) {
+		throw error(400, 'Captcha test failed, please try again or contact BTC Map.');
+	}
+
+	if (used.includes(captchaSecret)) {
+		throw error(400, 'Captcha has already been used, please try another.');
+	} else {
+		used.push(captchaSecret);
+	}
 
 	let github = await axios
 		.post(
