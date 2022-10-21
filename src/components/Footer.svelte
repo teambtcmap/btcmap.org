01d10899226a8d19e4fa65f21e7ccb7ebda65873
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-10-21 12:09:57 -0600

    make footer more responsive

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <footer class="w-full pb-5 flex justify-between items-center">
-	<div class="space-x-5">
+	<div class="space-x-2 md:space-x-5">
 		<SocialLink url={$socials.twitter} social="twitter" />
 		<SocialLink url={$socials.discord} social="discord" />
 		<SocialLink url={$socials.github} social="github" />
