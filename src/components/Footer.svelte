bf67301f4e917edffa0548b5a984e681284908a0
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-11-01 12:14:01 -0600

    add white paper to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,13 +3,14 @@
 	import { socials } from '$lib/store';
 </script>
 
-<footer class="w-full pb-5 flex justify-between items-center">
-	<div class="space-x-2 md:space-x-5">
+<footer class="w-full pb-5 md:flex justify-between items-center space-y-5 md:space-y-0">
+	<div class="space-x-5 flex md:block justify-center">
 		<SocialLink url={$socials.twitter} social="twitter" />
 		<SocialLink url={$socials.discord} social="discord" />
 		<SocialLink url={$socials.github} social="github" />
 	</div>
-	<div>
+	<div class="space-x-5 flex md:block justify-center">
+		<a href="/bitcoin.pdf" class="text-link hover:text-hover">White paper</a>
 		<a href="/privacy-policy" class="text-link hover:text-hover">Privacy</a>
 	</div>
 </footer>
