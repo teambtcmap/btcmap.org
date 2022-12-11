31c8e2d8a619b0c075d20333aabc562ff1b9c14d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-10 20:26:34 -0700

    style show tags modal content

--- src/components/ShowTags.svelte
+++ src/components/ShowTags.svelte
@@ -11,19 +11,21 @@
 	<OutClick excludeByQuerySelector={['#show-tags']} on:outclick={closeModal}>
 		<div
 			transition:fly={{ y: 200, duration: 300 }}
-			class="z-[2000] border border-mapBorder absolute top-[5vh] md:top-[calc(50vh-200px)] left-[5vw] md:left-[calc(50vw-215px)] h-[90vh] md:h-[400px] w-[90vw] md:w-[430px] bg-white p-6 rounded-xl shadow-2xl"
+			class="z-[2000] border border-mapBorder absolute top-[5vh] md:top-[calc(50vh-200px)] left-[5vw] md:left-[calc(50vw-215px)] h-[90vh] md:h-[400px] w-[90vw] md:w-[430px] bg-white p-6 rounded-xl shadow-2xl overflow-auto"
 		>
 			<CloseButton
-				position="absolute top-5 left-5"
+				position="flex justify-end"
 				click={closeModal}
 				colors="text-primary hover:text-link"
 			/>
-			<div class="break-all overflow-auto h-full w-full">
-				<pre>
-          <code>
-{JSON.stringify($showTags, null, 2)}
-          </code>
-        </pre>
+			<div class="space-y-2">
+				{#each Object.entries($showTags) as tag}
+					<div>
+						<span class="text-primary font-semibold">{tag[0]}</span>=<span
+							class="text-body font-semibold">{tag[1]}</span
+						>
+					</div>
+				{/each}
 			</div>
 		</div>
 	</OutClick>
