3a81b138891d98c516c203c66429e4c70095f840
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-22 10:56:16 -0700

    set font sizes in pixels

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte}'],
 	theme: {
+		fontSize: {
+			xs: '12px',
+			sm: '14px',
+			base: '16px',
+			lg: '18px',
+			xl: '20px',
+			'2xl': '24px',
+			'3xl': '30px',
+			'4xl': '36px',
+			'5xl': '48px',
+			'6xl': '60px',
+			'7xl': '72px',
+			'8xl': '96px',
+			'9xl': '128px'
+		},
 		extend: {
 			colors: {
 				primary: '#144046',
