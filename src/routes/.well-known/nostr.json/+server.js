154c61a07e2b829ec2481dc8fda80ae3487e7640
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2023-01-25 11:44:18 -0700

    add nathan nip05

--- src/routes/.well-known/nostr.json/+server.js
+++ src/routes/.well-known/nostr.json/+server.js
@@ -1,10 +1,11 @@
 export function GET() {
-  return new Response(
-    JSON.stringify({
-      names: {
-        _: "3eab247c63bb35dfa38e07ca102f6da28ba9b9d4687197743bde3a2b1d80aeed",
-      },
-    }),
-    { headers: { "Access-Control-Allow-Origin": "*" } }
-  );
+	return new Response(
+		JSON.stringify({
+			names: {
+				_: '3eab247c63bb35dfa38e07ca102f6da28ba9b9d4687197743bde3a2b1d80aeed',
+				nathan: 'c4f5e7a75a8ce3683d529cff06368439c529e5243c6b125ba68789198856cac7'
+			}
+		}),
+		{ headers: { 'Access-Control-Allow-Origin': '*' } }
+	);
 }
