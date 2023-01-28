f218d7eefa2bee6ed289ef6cb82111b7ebd222df
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2023-01-28 13:50:03 -0700

    add og tags to media page

--- src/routes/media/+page.svelte
+++ src/routes/media/+page.svelte
@@ -41,6 +41,13 @@
 	];
 </script>
 
+<svelte:head>
+	<title>BTC Map - Media</title>
+	<meta property="og:image" content="https://btcmap.org/images/og/home.png" />
+	<meta property="twitter:title" content="BTC Map - Media" />
+	<meta property="twitter:image" content="https://btcmap.org/images/og/home.png" />
+</svelte:head>
+
 <div class="bg-teal">
 	<Header />
 	<div class="w-10/12 xl:w-[1200px] mx-auto">
