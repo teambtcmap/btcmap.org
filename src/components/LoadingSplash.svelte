d3ffb44fccf44d8fde74eec93517dadc6b09ab04
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2023-02-11 18:08:23 -0700

    add loading animation to splash screen

--- src/components/LoadingSplash.svelte
+++ src/components/LoadingSplash.svelte
@@ -1,26 +1,33 @@
 <script>
 	export let page;
 
-	import { LoadingSpinner } from '$comp';
+	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
 </script>
 
 <div class="w-full bg-teal">
 	<div class="mx-auto w-10/12 xl:w-[1200px]">
 		<div class="flex h-[100vh] w-full items-center justify-center">
-			<main class="space-y-10">
-				<a href="/">
-					<img
-						decoding="sync"
-						fetchpriority="high"
-						src="/images/logo.svg"
-						alt="BTC Map"
-						class="mx-auto w-36 md:w-40"
-					/>
-				</a>
-				<LoadingSpinner color="text-link" size="w-16 h-16" style="mx-auto" />
-				<h1 class="text-center text-4xl font-semibold text-primary md:text-5xl">
+			<main class="space-y-10 text-center">
+				{#if typeof window !== 'undefined'}
+					<div class="mx-auto w-52">
+						<LottiePlayer
+							src="/lottie/loading.json"
+							autoplay={true}
+							loop={true}
+							controls={false}
+							renderer="svg"
+							background="transparent"
+						/>
+					</div>
+				{/if}
+				<h1 class="text-4xl font-semibold text-primary md:text-5xl">
 					Loading {page}...
 				</h1>
+				<a
+					href="/"
+					class="inline-block text-xl font-semibold text-link transition-colors hover:text-hover"
+					><i class="fa-solid fa-house mr-2" /> Home</a
+				>
 			</main>
 		</div>
 	</div>
