b5f10c63e7713bff56a0d21c9aca8d4bdae8a6ae
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2023-02-13 14:24:53 -0700

    fix footer responsiveness

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,18 +12,18 @@
 	];
 </script>
 
-<footer class="w-full items-center justify-between space-y-5 pb-5 md:flex md:space-y-0">
-	<div class="flex justify-center space-x-5 md:block">
+<footer class="w-full items-center justify-between space-y-5 pb-5 lg:flex lg:space-y-0">
+	<div class="flex justify-center space-x-5 lg:block">
 		<SocialLink url={$socials.twitter} social="twitter" />
 		<SocialLink url={$socials.discord} social="discord" />
 		<SocialLink url={$socials.github} social="github" />
 	</div>
-	<div class="flex flex-wrap justify-center md:block">
+	<div class="flex flex-wrap justify-center lg:block">
 		{#each links as link}
 			<a
 				href={link.link}
-				class="mx-2.5 {link.name !== 'Privacy'
-					? 'mb-2.5 md:mb-0'
+				class="mx-2.5 {link.name !== 'Cypherpunks'
+					? 'mb-2.5 lg:mb-0'
 					: ''} text-link transition-colors hover:text-hover">{link.name}</a
 			>
 		{/each}
