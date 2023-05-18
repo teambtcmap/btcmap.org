f0c4fb09674a825ce290c21985a23de2f133baa0
secondl1ght <secondl1ght@protonmail.com>
2023-05-18 00:27:12 -0600

    make same color on dark

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 				{/if}
 				<a
 					href="/map"
-					class="mx-auto my-8 block w-fit rounded-full bg-link px-7 py-3.5 text-lg font-semibold text-white transition-all hover:bg-hover dark:bg-gradient-to-r dark:from-[#0ecd71] dark:to-[#040273] dark:hover:bg-gradient-to-l"
+					class="mx-auto my-8 block w-fit rounded-full bg-link px-7 py-3.5 text-lg font-semibold text-white transition-colors hover:bg-hover"
 					>Open Map</a
 				>
 				<div
