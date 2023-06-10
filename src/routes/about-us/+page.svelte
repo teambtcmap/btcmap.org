14615c782fd4c4d86621037d84888f5db60342bc
secondl1ght <secondl1ght@protonmail.com>
2023-06-09 20:10:21 -0600

    add dashboard asset to prevent network fail

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -87,7 +87,7 @@
 		{ name: 'Bolt Card', icon: 'bolt', url: 'https://www.boltcard.org/' },
 		{ name: 'Galoy', icon: 'galoy', url: 'https://galoy.io/' },
 		{ name: 'Satimoto', icon: 'satimoto', url: 'https://satimoto.com/' },
-		{ name: 'Bitcoin Dashboard', icon: '', url: 'https://bitcoin-primodata.streamlit.app/' },
+		{ name: 'Bitcoin Dashboard', icon: 'dashboard', url: 'https://bitcoin-primodata.streamlit.app/' },
 		{ name: 'BitLocal', icon: 'bitlocal', url: 'https://www.bitlocal.app/' },
 		{ name: 'Nostr Bot', icon: 'nostr-bot', url: 'https://github.com/BcnBitcoinOnly/btcmap-bot' },
 		{ name: 'Fedi', icon: 'fedi', url: 'https://www.fedi.xyz/' },
