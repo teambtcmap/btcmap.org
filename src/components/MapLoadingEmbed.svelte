3a7114c7f3072a614c4534eaf5fcad8049fcf7ee
secondl1ght <secondl1ght@protonmail.com>
2023-12-13 20:40:45 -0700

    adjust map loader embed zindex

--- src/components/MapLoadingEmbed.svelte
+++ src/components/MapLoadingEmbed.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-	class="w-full {style} absolute left-0 top-0 z-[10000] flex items-center justify-center bg-teal dark:bg-dark"
+	class="w-full {style} absolute left-0 top-0 z-[25] flex items-center justify-center bg-teal dark:bg-dark"
 >
 	<div class="space-y-6">
 		<LoadingSpinner color="text-link" />
