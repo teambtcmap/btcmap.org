7c51f857e3121444abfc8a192992feec7c7b58d2
secondl1ght <secondl1ght@protonmail.com>
2024-05-25 12:35:50 -0600

    increase marker icon size for og images

--- src/components/MerchantOG.svelte
+++ src/components/MerchantOG.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <img
-	src="https://tiles.stadiamaps.com/static_cacheable/osm_bright.png?size=600x315@2x&markers={lat},{lon},custom:https://btcmap.org/icons/og-marker.png,,,,-16,-43&api_key={STADIA_API_KEY}"
+	src="https://tiles.stadiamaps.com/static_cacheable/osm_bright.png?size=600x315@2x&markers={lat},{lon},custom:https://btcmap.org/icons/og-marker.png,,,,-32,-86&api_key={STADIA_API_KEY}"
 	alt="merchant location"
 	width="1200"
 	height="630"
