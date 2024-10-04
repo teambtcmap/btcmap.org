ec97c5309a6ba9ae759c56c450947e67a6503de1
Nathan Day <87125117+dadofsambonzuki@users.noreply.github.com>
2024-10-04 13:46:19 +0000

    Added NIP-05 identifier for Rockedf.
    Plus .replit config file.

--- src/routes/.well-known/nostr.json/+server.ts
+++ src/routes/.well-known/nostr.json/+server.ts
@@ -3,7 +3,8 @@ export function GET() {
 		JSON.stringify({
 			names: {
 				_: '3eab247c63bb35dfa38e07ca102f6da28ba9b9d4687197743bde3a2b1d80aeed',
-				nathan: 'c4f5e7a75a8ce3683d529cff06368439c529e5243c6b125ba68789198856cac7'
+				nathan: 'c4f5e7a75a8ce3683d529cff06368439c529e5243c6b125ba68789198856cac7',
+				rockedf: '7e9a12f456e1666dd0c358ed3ce579e020286ee9b64d2c9bdb9a907e47017de6'
 			}
 		}),
 		{ headers: { 'Access-Control-Allow-Origin': '*' } }
