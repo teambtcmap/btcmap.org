5943cebd2c789ee8535f3be181a4dfe7c2ecbd79
escapedcat <github@htmlcss.de>
2025-02-23 13:58:00 +0100

    chore: start server for playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,12 +68,12 @@ export default defineConfig({
 		//   name: 'Google Chrome',
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
-	]
+	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	// 	command: 'yarn dev',
-	// 	url: 'http://127.0.0.1:5173',
-	// 	reuseExistingServer: !process.env.CI
-	// }
+	webServer: {
+		command: 'yarn dev',
+		url: 'http://127.0.0.1:5173',
+		reuseExistingServer: !process.env.CI
+	}
 });
