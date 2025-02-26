b199b0888082e91e63151300a0190cb7937967e0
escapedcat <github@htmlcss.de>
2025-02-26 19:21:04 +0100

    refactor(icons): use npm module to handle sprite generation and add draft usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), VitePluginSvgSpritemap('./src/lib/icons/**/*.svg')],
 	server: {
 		host: '0.0.0.0',
 		allowedHosts: ['btcmap.org']
