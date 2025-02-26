c8eb9372b10ff246e58ef9c79f7994461dc10e18
escapedcat <github@htmlcss.de>
2025-02-26 16:28:53 +0100

    chore: removed unused OG related deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: { exclude: ['@resvg/resvg-js'] },
 	server: {
 		host: '0.0.0.0',
 		allowedHosts: ['btcmap.org']
