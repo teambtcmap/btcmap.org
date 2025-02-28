a35a55ede47720d14a34a9d1cb2b32c8e3352dc3
escapedcat <github@htmlcss.de>
2025-02-28 19:29:48 +0100

    refactor(icons): export popup icons

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		VitePluginSvgSpritemap(['./static/icons/socials/*.svg', './static/icons/apps/*.svg'])
+		VitePluginSvgSpritemap([
+			'./static/icons/socials/*.svg',
+			'./static/icons/apps/*.svg',
+			'./static/icons/popup/*.svg'
+		])
 	],
 	server: {
 		host: '0.0.0.0',
