fb5d4af955bdbab840b8d812b8a821acef20f28a
escapedcat <github@htmlcss.de>
2025-03-24 19:21:50 +0100

    feat(icons): switch material icons to a more extensive set

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -25,7 +25,8 @@
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const props = { type, icon, w, h, style } as IconProps;
 
-	$: formattedMaterialIcon = type === 'material' ? `ic:outline-${icon.replace(/_/g, '-')}` : icon;
+	$: formattedMaterialIcon =
+		type === 'material' ? `material-symbols:${icon.replace(/_/g, '-')}-outline` : icon;
 
 	$: spriteHref =
 		{
