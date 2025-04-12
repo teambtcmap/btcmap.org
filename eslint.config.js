7af0aa9a4214943264ee838778fb2b868ef0fc28
escapedcat <github@htmlcss.de>
2025-04-12 12:27:02 +0200

    chore: add globals to eslint config to i.e. support console

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import eslintPluginSvelte from 'eslint-plugin-svelte';
 import * as svelteParser from 'svelte-eslint-parser';
 import * as typescriptParser from '@typescript-eslint/parser';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default tseslint.config(
 	js.configs.recommended,
@@ -23,6 +24,10 @@ export default tseslint.config(
 				parser: typescriptParser,
 				project: './tsconfig.json',
 				extraFileExtensions: ['.svelte']
+			},
+			globals: {
+				...globals.browser,
+				...globals.node
 			}
 		},
 		rules: {
