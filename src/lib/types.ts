12772106d812df9342e8033f9f9b356fda2a9fd8
escapedcat <github@htmlcss.de>
2025-07-08 16:48:32 +0200

    feat(map): reduce marker and popup for POC #276

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,7 +2,6 @@ import type { GeoJSON, Polygon, MultiPolygon } from 'geojson';
 import type leaflet from 'leaflet';
 import type {
 	DomEvent,
-	DivIcon,
 	FeatureGroup,
 	LatLng,
 	LayerGroup,
@@ -173,19 +172,6 @@ export type IssueIcon =
 	| 'fa-hourglass-end'
 	| 'fa-list-check'
 	| 'fa-hourglass-half';
-
-export interface GenerateMarkerParams {
-	lat: number;
-	long: number;
-	icon: DivIcon;
-	element: ElementOSM;
-	payment: PayMerchant;
-	leaflet: Leaflet;
-	verifiedDate: number;
-	verify: boolean;
-	boosted?: string;
-	issues?: Issue[];
-}
 
 export type RpcGetMostActiveUsersItem = {
 	id: number;
