6fac9d5e3482f42c22101da1c2ef05b69069fa95
Joseph King <jking@Josephs-MacBook-Pro.local>
2025-11-18 16:18:36 -0500

    center top button

--- src/components/TopButton.svelte
+++ src/components/TopButton.svelte
@@ -6,7 +6,7 @@
 
 <button
 	on:click={() => (scroll ? scroll : window).scrollTo({ top: 0, behavior: 'smooth' })}
-	class="h-8 w-8 animate-bounce rounded-full bg-link text-white hover:bg-hover {style} transition-colors"
+	class="h-8 w-8 animate-bounce rounded-full bg-link text-white hover:bg-hover {style} transition-colors flex items-center justify-center"
 >
 	<Icon type="fa" icon="chevron-up" w="16" h="16" />
 </button>
