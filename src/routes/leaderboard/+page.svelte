b8bf93ab917281e3f7c3049c086b83bbb355bb78
Nathan Day <87125117+dadofsambonzuki@users.noreply.github.com>
2025-12-15 19:54:15 +0000

    style: applied sam dropdown styling as other pages

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -434,6 +434,17 @@
 	</div>
 </main>
 
+{#if typeof window !== 'undefined'}
+	{#if detectTheme() === 'dark' || $theme === 'dark'}
+		<style>
+			select option {
+				--tw-bg-opacity: 1;
+				background-color: rgb(55 65 81 / var(--tw-bg-opacity));
+			}
+		</style>
+	{/if}
+{/if}
+
 <style>
 	#hero {
 		background-image: url('/images/confetti.png');
