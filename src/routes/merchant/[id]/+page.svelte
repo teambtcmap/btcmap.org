c403e4e28cc8f4c7c755e985088cb0cbb91e4e6e
daviluquini <davi262016@gmail.com>
2026-05-04 10:48:39 -0300

    fix(merchant): only render tag issues icon when issues exist (#965)

--- src/routes/merchant/[id]/+page.svelte
+++ src/routes/merchant/[id]/+page.svelte
@@ -502,13 +502,15 @@ const ogImage = `https://api.btcmap.org/og/element/${data.id}`;
 					/>
 				</span>
 
-				<span id="tagging-issues">
-					<MerchantAction
-						on:click={() => ($taggingIssues = data.osmTags?.issues || [])}
-						icon="issues"
-						text={$_('merchant.tagIssues')}
-					/>
-				</span>
+				{#if data.osmTags?.issues?.length}
+					<span id="tagging-issues">
+						<MerchantAction
+							on:click={() => ($taggingIssues = data.osmTags.issues)}
+							icon="issues"
+							text={$_('merchant.tagIssues')}
+						/>
+					</span>
+				{/if}
 
 				<MerchantAction
 					link={`${data.osmViewUrl}`}
